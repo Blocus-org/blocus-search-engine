5586434143070106e8b477e9370577969d6b2337
Zqfd <zqfdrshj@protonmail.com>
2023-03-21 08:23:44 +0100

    fixes Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

--- donate.php
+++ donate.php
@@ -8,13 +8,10 @@
     </head>
     <body>
         <div class="misc-container">
-               <h1>Donate to the original source developer</h1>
-               <h2>Bitcoin (BTC):</h2>
-               <p>bc1qs43kh6tvhch02dtsp7x7hcrwj8fwe4rzy7lp0h<br><br><a href="static/images/btc.png">QR code</a></p>
-
-               <h2>Monero (XMR):</h2>
-               <p>41dGQr9EwZBfYBY3fibTtJZYfssfRuzJZDSVDeneoVcgckehK3BiLxAV4FvEVJiVqdiW996zvMxhFB8G8ot9nBFqQ84VkuC<br><br><a href="static/images/xmr.png">QR code</a></p>
-                <h1>Donate to the Blocus developer</h1>
+               <br><br><h1>Donate to the original source developer</h1>
+               <p>Bitcoin (BTC): bc1qs43kh6tvhch02dtsp7x7hcrwj8fwe4rzy7lp0h<br><br><a href="static/images/btc.png">QR code</a></p>
+                <p>Monero (XMR): 41dGQr9EwZBfYBY3fibTtJZYfssfRuzJZDSVDeneoVcgckehK3BiLxAV4FvEVJiVqdiW996zvMxhFB8G8ot9nBFqQ84VkuC<br><br><a href="static/images/xmr.png">QR code</a></p> 
+                <a href="https://patreon.com/Blocus"><h1>Donate to the Blocus developer</h1></a>
               
         </div>
 
